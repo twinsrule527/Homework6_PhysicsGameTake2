85485ff442d285d2536467cf0ac75708619140ab
Eli <eliclendenning@gmail.com>
2021-04-05 10:30:44 -0500

    Added instructions and Endgame

---
+++ Assets/Scripts/EndGameTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//Script that triggers when the player ends the game giving them an overlay
+public class EndGameTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject EndGameScreen;
+    void OnTriggerEnter(Collider activator) {
+        EndGameScreen.SetActive(true);
+    }
+}
